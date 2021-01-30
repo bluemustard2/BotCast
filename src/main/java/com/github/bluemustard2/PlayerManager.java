492131e2b94f1372d30ea51c2438e1d37761742c
simpleauthority <jacob@algorithmjunkie.com>
2021-01-30 02:32:39 -0800

    go go go

--- src/main/java/com/github/bluemustard2/PlayerManager.java
+++ src/main/java/com/github/bluemustard2/PlayerManager.java
@@ -1,81 +1,136 @@
 package com.github.bluemustard2;
 
-import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.event.TrackEndEvent;
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
-import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
-import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import org.javacord.api.DiscordApi;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import org.javacord.api.audio.AudioSource;
-import org.javacord.api.entity.channel.ServerVoiceChannel;
 
-import java.util.Optional;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
 
 public class PlayerManager {
+    private final Main main;
+    private final Deque<AudioTrack> trackQueue;
+    private final AudioPlayerManager backend;
+    private final AudioPlayer player;
+    private final AudioSource audioSource;
+    private final CoolAudioLoadResultHandler loadResultHandler;
 
-    protected static String line;
-    protected static AudioSource source;
+    public PlayerManager(Main main) {
+        this.main = main;
 
-    public AudioPlayer Manager() {
-        AudioPlayerManager playerManager = new DefaultAudioPlayerManager();
-        playerManager.registerSourceManager(new YoutubeAudioSourceManager());
-        AudioPlayer player = playerManager.createPlayer();
+        // create a queue object
+        trackQueue = new ArrayDeque<>();
 
-        Optional<ServerVoiceChannel> channel = api.getServerVoiceChannelById(System.getenv("SERVER_ID"));
+        // create the backend stuff for LavaPlayer
+        backend = new DefaultAudioPlayerManager();
+        backend.registerSourceManager(new YoutubeAudioSourceManager());
 
-        if (channel.isPresent()) {
-            ServerVoiceChannel unwrapped = channel.get();
+        // create the actual Player and add a Listener to play the next track in the queue, if the queue is not empty.
+        player = backend.createPlayer();
+        player.addListener(event -> {
+            // is the audio event we got a track ending?
+            if (event instanceof TrackEndEvent) {
+                TrackEndEvent endEvent = (TrackEndEvent) event;
 
-            unwrapped.getId();
-            unwrapped.connect().thenAccept(audioConnection -> {
-                source = new LavaplayerAudioSource(api, player);
-                audioConnection.setAudioSource(source);
-            }).exceptionally(e -> {
-                e.printStackTrace();
-                return null;
-            });
+                // was it ended forcefully or did it naturally finish the song?
+                if (endEvent.endReason == AudioTrackEndReason.FINISHED) {
 
-            api.addMessageCreateListener(event -> {
-                String[] messageContent = event.getMessageContent().split(" ");
+                    // get the next track in the queue, if there is none dip out
+                    AudioTrack nextTrack = trackQueue.poll();
+                    if (nextTrack == null) {
+                        return;
+                    }
 
-                line = YouTubeSearch.Search(messageContent);
-
-                if (messageContent[0].equalsIgnoreCase("!play")) {
-                    playerManager.loadItem(line, new AudioLoadResultHandler() {
-                        @Override
-                        public void trackLoaded(AudioTrack track) {
-                            player.playTrack(track);
-                        }
-
-                        @Override
-                        public void playlistLoaded(AudioPlaylist playlist) {
-                            for (AudioTrack track : playlist.getTracks()) {
-                                player.playTrack(track);
-                            }
-                        }
-
-                        @Override
-                        public void noMatches() {
-                            System.out.println("Song could not be found!");
-                        }
-
-                        @Override
-                        public void loadFailed(FriendlyException e) {
-                            e.printStackTrace();
-                        }
-                    });
-                } else if (messageContent[0].equalsIgnoreCase("!stop")) {
-                    player.stopTrack();
-                } else if (messageContent[0].equalsIgnoreCase("!pause")) {
-                    player.setPaused(true);
-                } else if (messageContent[0].equalsIgnoreCase("!resume")) {
-                    player.setPaused(false);
+                    // play the next track
+                    playTrack(nextTrack);
                 }
-            });
-        }
-        return player;
+            }
+        });
+
+        // create the audio source that we will send to Discord. This is automatically linked to whatever the player
+        // is doing
+        audioSource = new LavaPlayerAudioSource(main.getDiscordApi(), player);
+
+        // create the handler that deals with results loaded from YouTube
+        loadResultHandler = new CoolAudioLoadResultHandler(main, this);
+    }
+
+    // This is literally the same thing as you had in that giant if for playing a song
+    public void searchForAndQueueSong(List<String> searchTerms) {
+        backend.loadItem(YouTubeSearch.search(searchTerms.toArray(String[]::new)), loadResultHandler);
+    }
+
+    public boolean isCurrentlyPlaying() {
+        // how might we check if it's currently playing something?
+        // don't overthink it
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    public void playTrack(AudioTrack track) {
+        // play the given track plz
+        // if there is already something playing, add it to the queue instead!
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    /**
+     * Adds the given track to the queue
+     *
+     * @param track the track to add
+     */
+    public void addTrackToQueue(AudioTrack track) {
+        trackQueue.add(track);
+    }
+
+    /**
+     * Adds all the tracks to the queue
+     *
+     * @param playlist the list of tracks to add
+     */
+    public void addTracksToQueue(List<AudioTrack> playlist) {
+        trackQueue.addAll(playlist);
+    }
+
+    public void resumePlaying() {
+        // start playing again!!!!! this assumes it was paused before.
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    public void pausePlaying() {
+        // stop playing immediately!!!!!!!!!! or puppies die.
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    public void clearQueue() {
+        // clear the queue so that nothing else will play after the current song is done playing
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    public void skipSong() {
+        // skip the current song, if there are no more songs stop playing
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    /**
+     * Gets the Main class so we can use it elsewhere
+     *
+     * @return the main class
+     */
+    public Main getMain() {
+        return main;
+    }
+
+    /**
+     * Gets the AudioSource that we can give to Discord
+     *
+     * @return the audio source
+     */
+    public AudioSource getAudioSource() {
+        return audioSource;
     }
 }
